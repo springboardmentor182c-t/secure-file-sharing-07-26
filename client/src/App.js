7e677b4619c5df0075480af32d9f991d907d6334
sushmitahukkerimath <sshukkerimath365@gmail.com>
2026-07-12 23:35:03 +0530

    Completed dashboard UI layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageContainer from "./layout/PageContainer";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import SecureSharing from "./pages/Securesharing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Secure File Sharing - Client</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PageContainer>
+              <Home />
+            </PageContainer>
+          }
+        />
+
+        <Route
+          path="/sharing"
+          element={
+            <PageContainer>
+              <SecureSharing />
+            </PageContainer>
+          }
+        />
+
+        <Route
+          path="/settings"
+          element={
+            <PageContainer>
+              <Settings />
+            </PageContainer>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
