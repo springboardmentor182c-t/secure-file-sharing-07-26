42eb576e3bfff7d8bb403fba25866bd4d2132a54
shashankkumar2512 <shashankkumar251204@gmail.com>
2026-07-20 21:49:42 +0530

    Add shared entities back to init

--- server/src/entities/__init__.py
+++ server/src/entities/__init__.py
@@ -1,20 +1,22 @@
 from src.entities.base import Base
 from src.entities.user import User
-from src.entities.folder import Folder      # <-- ADD THIS
+from src.entities.folder import Folder
 from src.entities.file import File
 from src.entities.shared_link import SharedLink
 from src.entities.access_log import AccessLog
 from src.entities.notification import Notification
+from src.entities.file_share import FileShare
 from src.entities.file_category import FileCategory
 
 
 __all__ = [
     "Base",
     "User",
-    "Folder",      # <-- ADD THIS
+    "Folder",
     "File",
     "SharedLink",
     "AccessLog",
     "Notification",
+    "FileShare",
     "FileCategory",
 ]
