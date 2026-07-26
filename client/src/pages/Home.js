ec985b5f18755ed36d4795e3e58a5ac8d6baec4a
sushmitahukkerimath <sshukkerimath365@gmail.com>
2026-07-26 22:43:00 +0530

    Fix Home page and resolve CSS issues

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,10 @@
 import React from "react";
 
-
 const Home = () => {
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-                fontSize: "32px",
-                fontWeight: "bold"
-            }}
-        >
-            Login Successful ✅
+        <div>
         </div>
     );
 };
-
-
-const Home=()=>{
-
-return(
-<div>
-</div>
-)
-
-}
-
-
 
 export default Home;
